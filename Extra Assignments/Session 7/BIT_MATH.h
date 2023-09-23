d938b6e33cbb1313a905e70a9b991856142fde3a
Abdelrahman Moataz <143439943+AbdelrahmanMoataz@users.noreply.github.com>
2023-09-23 13:43:04 +0300

    Update BIT_MATH.h

--- Extra Assignments/Session 7/BIT_MATH.h
+++ Extra Assignments/Session 7/BIT_MATH.h
@@ -1,7 +1,7 @@
 #define SET_BIT(b,n) (b) |= 1 << (n)
 #define CLEAR_BIT(b,n) (b) &= ~(1 << (n))
 #define TOGGLE_BIT(b,n) (b) ^= 1 << (n)
-#define GET_BIT(b,n) (b >> n) & 1
+#define GET_BIT(b,n) ((b) >> (n)) & 1
 
 // Shift to right n times means nth bit goes to bit 1,
 // then AND with 0000 0001 to get state of bit
